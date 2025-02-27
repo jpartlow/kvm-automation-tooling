e95a27ac1d7eec2f83287dab325f9e4542006711
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-02-27 13:32:35 -0800

    wip separate Client class for libvirt
    add create_pool pool_exist?  functions

--- lib/kvm_automation_tooling/libvirt.rb
+++ lib/kvm_automation_tooling/libvirt.rb
@@ -1,22 +1,31 @@
+require 'etc'
 require 'libvirt'
 
 module KvmAutomationTooling
-  module Libvirt
-    # https://gitlab.com/libvirt/libvirt-ruby/-/blob/master/examples/upload_volume.rb
-    def upload_volume(file_path, volume_name, capacity = 3)
-      with_libvirt do |lv|
-        # define the XML that describes the new volume
-        vol_xml = <<~EOF
+  # Library for interacting with libvirt through the ruby-libvert gem.
+  module LibvirtWrapper
+
+    # Wrapper around the libvirt gem to provide a more user-friendly
+    # interface.
+    class Client
+      attr_reader :lv
+
+      def initialize
+        @lv = Libvirt::open("qemu:///system")
+      end
+
+      # https://gitlab.com/libvirt/libvirt-ruby/-/blob/master/examples/upload_volume.rb
+      def upload_volume(file_path, volume_name, capacity = 3)
+        # get a reference to the default storage pool
+        pool = lv.lookup_storage_pool_by_name("default")
+        # create the new volume in the storage pool
+        volume = pool.create_volume_xml(<<~EOF)
           <volume>
             <name>#{volume_name}</name>
             <allocation unit="b">#{File.size(file_path)}</allocation>
             <capacity unit="G">#{capacity}</capacity>
           </volume>
         EOF
-        # get a reference to the default storage pool
-        pool = lv.lookup_storage_pool_by_name("default")
-        # create the new volume in the storage pool
-        volume = pool.create_volume_xml(vol_xml)
         # open up the original file
         image_file = File.open(file_path, "rb")
         # create a new stream to upload the data
@@ -25,13 +34,14 @@ module KvmAutomationTooling
         volume.upload(stream, 0, image_file.size)
         error = nil
         begin
-          # send all of the data over the stream.  For each invocation of the
-          # block, ruby-libvirt yields a tuple containing the opaque data passed
-          # into sendall (here, nil), and the maximum number of bytes that it is
-          # willing to accept right now.  The block should return a tuple, where
-          # the first argument returns the number of bytes actually filled in
-          # (up to a maximum of 'n', and with 0 meaning EOF), and the second
-          # argument being the string containing the data to send.
+          # send all of the data over the stream.  For each invocation of
+          # the block, ruby-libvirt yields a tuple containing the opaque
+          # data passed into sendall (here, nil), and the maximum number
+          # of bytes that it is willing to accept right now.  The block
+          # should return a tuple, where the first argument returns the
+          # number of bytes actually filled in (up to a maximum of 'n',
+          # and with 0 meaning EOF), and the second argument being the
+          # string containing the data to send.
           stream.sendall do |_opaque, n|
             begin
               r = image_file.read(n)
@@ -43,33 +53,75 @@ module KvmAutomationTooling
           end
   
           raise error if error
+
+          return true
         ensure
-          # once all of the data has been read by the block above, finish *must*
-          # be called to ensure that all of it gets uploaded
+          # once all of the data has been read by the block above, finish
+          # *must* be called to ensure that all of it gets uploaded
           error.nil? ? stream.finish : stream.abort
         end
       end
-    end
   
+      def volume_exist?(volume_name)
+        pool = lv.lookup_storage_pool_by_name("default")
+        pool.list_volumes.include?(volume_name)
+      end
+
+      # Define and create a persistent directory storage pool as a
+      # subdirectory of the default storage pool.
+      #
+      # @param pool_name [String] The name of the pool to create.
+      # @param default_pool_path [String] The path to the default storage
+      # pool.
+      # @param mode [Integer] The permissions mode for the pool directory.
+      # @param uid [Integer] The owner uid of the pool directory.
+      # @param gid [Integer] The group id of the pool directory. Defaults
+      # to libvirt if the group exists, otherwise root.
+      def create_pool(pool_name, default_pool_path: "/var/lib/libvirt/images", mode: '0750', uid: 0, gid: nil)
+        if gid.nil?
+          group = Etc.getgrnam('libvirt')
+          gid = !group.nil? ? group.gid : 0
+        end
+
+        pool = lv.define_storage_pool_xml(<<~EOF)
+          <pool type='dir'>
+            <name>#{pool_name}</name>
+            <target>
+              <path>#{default_pool_path}/#{pool_name}</path>
+              <permissions>
+                <mode>#{mode}</mode>
+                <owner>#{uid}</owner>
+                <group>#{gid}</group>
+              </permissions>
+            </target>
+          </pool>
+        EOF
+        pool.build
+        pool.create
+        pool.autostart = true
+      end
+
+      def pool_exist?(pool_name)
+        lv.list_storage_pools.include?(pool_name)
+      end
+
+      def close
+        lv.close
+      end
+    end
+
+    # Entry point for the libvirt wrapper, yields a Client instance with a
+    # libvirt connection to the passed block.
     def with_libvirt(&block)
       if @libvirt
         yield(@libvirt)
       else
-        lv = Libvirt::open("qemu:///system")
-        @libvirt = lv
+        @libvirt = Client.new
         begin
           yield(@libvirt)
         ensure
-          @libvirt = nil
-          lv.close
+          @libvirt.close
         end
-      end
-    end
-  
-    def volume_exist?(volume_name)
-      with_libvirt do |lv|
-        pool = lv.lookup_storage_pool_by_name("default")
-        pool.list_volumes.include?(volume_name)
       end
     end
   end
--- spec/unit/kvm_automation_tooling/libvirt_spec.rb
+++ spec/unit/kvm_automation_tooling/libvirt_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-require_relative '../../tasks/import_libvirt_volume'
+require 'kvm_automation_tooling/libvirt'
 
 class LibvirtTest
-  # Specs manipulating the local libvirt environment are unexpected.
-  # To run these specs, set the RUN_KVM_AUTOMATION_TOOLING_LIBVIRT_SPECS environment variable.
+  # Specs manipulating the local libvirt environment are unexpected.  To
+  # run these specs, set the RUN_KVM_AUTOMATION_TOOLING_LIBVIRT_SPECS
+  # environment variable.
   RUN_LIBVIRT_SPECS = ENV['RUN_KVM_AUTOMATION_TOOLING_LIBVIRT_SPECS']
 
   def self.libvirt_available?
@@ -14,39 +15,96 @@ class LibvirtTest
   end
 end
 
-describe 'task: import_libvirt_volume.rb', if: LibvirtTest.libvirt_available? do
+describe KvmAutomationTooling::LibvirtWrapper, if: LibvirtTest.libvirt_available? do
   let(:libvirt) { Libvirt::open("qemu:///system") }
   let(:pool) { libvirt.lookup_storage_pool_by_name("default") }
   let(:volumes) { pool.list_volumes }
-  let(:task) { ImportLibvirtVolume.new }
 
-  describe '#upload_volume' do
-    let(:test_volume_name) { 'kvm-automation-tooling-spec-test-volume' }
-    let(:test_image) { '/tmp/kvm-automation-tooling-spec-test-volume.img' }
-
-    around(:each) do |example|
-      File.write(test_image, 'test')
-      example.run
-    ensure
-      File.delete(test_image) if File.exist?(test_image)
-      vol = pool.lookup_volume_by_name(test_volume_name)
-      vol.delete if vol
+  describe 'Client' do
+    let(:client) { KvmAutomationTooling::LibvirtWrapper::Client.new }
+  
+    describe '#upload_volume' do
+      let(:test_volume_name) { 'kvm-automation-tooling-spec-test-volume' }
+      let(:test_image) { '/tmp/kvm-automation-tooling-spec-test-volume.img' }
+  
+      around(:each) do |example|
+        File.write(test_image, 'test')
+        example.run
+      ensure
+        File.delete(test_image) if File.exist?(test_image)
+        vol = pool.lookup_volume_by_name(test_volume_name)
+        vol.delete if vol
+      end
+  
+      it 'uploads a volume' do
+        expect(client.volume_exist?(test_volume_name)).to eq(false)
+        expect(client.upload_volume(test_image, test_volume_name)).to eq(true)
+        expect(client.volume_exist?(test_volume_name)).to eq(true)
+      end
+    end
+  
+    describe '#volume_exist?' do
+      it 'returns true if the volume exists' do
+        expect(client.volume_exist?(volumes.first)).to eq(true)
+      end
+  
+      it 'returns false if the volume does not exist' do
+        expect(client.volume_exist?('a-thousand-dingos')).to eq(false)
+      end
     end
 
-    it 'uploads a volume' do
-      expect(task.volume_exist?(test_volume_name)).to eq(false)
-      expect(task.upload_volume(test_image, test_volume_name)).to be_nil
-      expect(task.volume_exist?(test_volume_name)).to eq(true)
+    describe '#pool_exist?' do
+      it 'returns true if the pool exists' do
+        expect(client.pool_exist?('default')).to eq(true)
+      end
+
+      it 'returns false if the pool does not exist' do
+        expect(client.pool_exist?('two-thousand-dingos')).to eq(false)
+      end
+    end
+
+    describe '#create_pool' do
+      let(:test_pool_name) { 'kvm-automation-tooling-spec-test-pool' }
+  
+      around(:each) do |example|
+        example.run
+      ensure
+        begin
+          test_pool = libvirt.lookup_storage_pool_by_name(test_pool_name)
+          test_pool.destroy
+          test_pool.delete
+          test_pool.undefine
+        rescue Libvirt::RetrieveError
+          # Thrown by lookup_storage_pool_by_name if the pool does not
+          # exist...
+        end
+      end
+  
+      it 'creates a pool' do
+        expect(client.pool_exist?(test_pool_name)).to eq(false)
+        expect(client.create_pool(test_pool_name)).to eq(true)
+        expect(client.pool_exist?(test_pool_name)).to eq(true)
+        pool = libvirt.lookup_storage_pool_by_name(test_pool_name)
+        expect(pool.active?).to eq(true)
+        expect(pool.autostart).to eq(true)
+      end
     end
   end
 
-  describe '#volume_exist?' do
-    it 'returns true if the volume exists' do
-      expect(task.volume_exist?(volumes.first)).to eq(true)
+  describe '#with_libvirt' do
+    class Tester
+      include KvmAutomationTooling::LibvirtWrapper
     end
 
-    it 'returns false if the volume does not exist' do
-      expect(task.volume_exist?('a-thousand-dingos')).to eq(false)
+    let(:tester) { Tester.new }
+
+    it 'yields a Client instance with a libvirt connection' do
+      c = nil
+      tester.with_libvirt do |client|
+        expect(client.volume_exist?(volumes.first)).to eq(true)
+        c = client
+      end
+      expect(c.lv).to be_closed
     end
   end
 end
