d204ef2d9060f2de38cb5e1e7e9d6e749ce00863
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-02-28 15:15:39 -0800

    wip get standup_cluster plan passing a basic spec

--- spec/plans/standup_cluster_spec.rb
+++ spec/plans/standup_cluster_spec.rb
@@ -1,23 +1,45 @@
 require 'spec_helper'
+require 'tmpdir'
 
 describe 'plan: standup_cluster' do
   include_context 'plan_init'
 
+  let(:terraform_state_dir) { Dir.mktmpdir('rspec-kat-terraform-state') }
   let(:params) do
     {
-      'cluster_name' => 'test_cluster',
-      'os' => 'ubuntu',
-      'os_version' => '24.04',
-      'os_arch' => 'x86_64',
+      'cluster_name' => 'spec',
+      'os'           => 'ubuntu',
+      'os_version'   => '24.04',
+      'os_arch'      => 'x86_64',
+      'terraform_state_dir' => terraform_state_dir,
     }
   end
-require 'pry-byebug'
+  let(:cluster_id) { 'spec-singular-ubuntu-2404-amd64' }
+
+  around(:each) do |example|
+    example.run
+  ensure
+    FileUtils.remove_entry_secure(terraform_state_dir)
+  end
+
   it 'should run successfully' do
     expect_task('kvm_automation_tooling::download_image')
     expect_task('kvm_automation_tooling::import_libvirt_volume')
     expect_task('kvm_automation_tooling::create_libvirt_image_pool')
+    expect_task('terraform::initialize')
+    expect_task('terraform::apply')
+      .with_targets('localhost')
+      .with_params(
+        'dir'       => './terraform',
+        'state'     => "#{terraform_state_dir}/#{cluster_id}.tfstate",
+        'state_out' => nil,
+        'target'    => nil,
+        'var'       => nil,
+        'var_file'  => "#{terraform_state_dir}/#{cluster_id}.tfvars.json",
+      )
 
     result = run_plan('kvm_automation_tooling::standup_cluster', params)
+    puts %x[ls -l #{terraform_state_dir}]
     expect(result.ok?).to(eq(true), result.value)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,20 +2,48 @@
 
 require 'rspec-puppet'
 
-fixture_path = File.expand_path(File.join(__dir__, 'fixtures'))
+module KatRspec
+  def self.fixture_path
+    File.expand_path(File.join(__dir__, 'fixtures'))
+  end
+
+  def self.modulepath
+    [
+      # spec/fixtures/modules is where rspec-puppet autogenerates
+      # a symlink for the module being tested
+      File.join(fixture_path, 'modules'),
+      # .modules is where bolt puts its modules (all of our dependencies)
+      File.join(__dir__, '..', '.modules'),
+    ]
+  end
+end
 
 RSpec.configure do |c|
-  # Include the Bolt .modules directory as part of the modulepath for dependencies.
-#  c.module_path     = [File.join(fixture_path, 'modules'), File.join(__dir__, '..', '.modules')].join(':')
-  c.module_path     = File.join(fixture_path, 'modules')
-  c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
-  c.environmentpath = File.join(Dir.pwd, 'spec')
+  # Include the Bolt .modules directory as part of the modulepath for
+  # dependencies. Joined because rspec-puppet can only deal with a
+  # multi-element modulepath as a string.
+  c.module_path     = KatRspec.modulepath.join(File::PATH_SEPARATOR)
+  c.manifest        = File.join(KatRspec.fixture_path, 'manifests', 'site.pp')
+  c.environmentpath = File.join(Dir.pwd, 'spec', 'environments')
 end
 
 require 'bolt_spec/plans'
 
 RSpec.shared_context 'plan_init' do
   include BoltSpec::Plans
+
+  # This should still execute before the before(:all)
+  # See: https://rspec.info/features/3-12/rspec-core/hooks/around-hooks/
+  around(:example) do |example|
+    old_modpath = RSpec.configuration.module_path
+    # This bit of insanity is due to the fact that rspec-puppet can only
+    # deal with a module:path, while BoltSpec can only deal with a
+    # [module, path]...
+    RSpec.configuration.module_path = KatRspec.modulepath
+    example.run
+  ensure
+    RSpec.configuration.module_path = old_modpath
+  end
 
   before(:all) do
     BoltSpec::Plans.init
