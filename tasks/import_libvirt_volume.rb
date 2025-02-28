b25aa3667a512a8eea9a7c355de22571c80f0e89
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-02-27 16:48:24 -0800

    f import_libvirt_volume task
    adds spec

--- tasks/import_libvirt_volume.rb
+++ tasks/import_libvirt_volume.rb
@@ -1,17 +1,24 @@
 #! /usr/bin/env ruby
 
 require_relative "../../ruby_task_helper/files/task_helper.rb"
-require_relative "../lib/kvm_automation_tooling/libvirt.rb"
+require_relative "../lib/kvm_automation_tooling/libvirt_wrapper.rb"
 
 class ImportLibvirtVolume < TaskHelper
-  include KvmAutomationTooling::Libvirt
+  include KvmAutomationTooling::LibvirtWrapper
 
   def task(image_path:, volume_name:, **kwargs)
-    with_libvirt do
-      if !volume_exist?(volume_name)
-        upload_volume(volume_name, image_path)
+    created = false
+    with_libvirt do |lv|
+      if !lv.volume_exist?(volume_name)
+        lv.upload_volume(volume_name, file_path: image_path)
+        created = true
       end
     end
+
+    {
+      status: 'success',
+      created: created,
+    }
   end
 end
 
