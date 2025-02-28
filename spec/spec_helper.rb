2a8278361a9d019dfdd15cba9c8602b45217530a
Josh Partlow <joshua.partlow@puppetlabs.com>
2025-02-27 16:51:17 -0800

    f spec_helper changes for running plans
    Having trouble with the fact that you can only set a single directory
    for modulepath. However need both the spec/fixtures/modules for the
    symlink to the module itself, and .modules/ for supporting modules like
    stdlib installed by bolt module install. Unless I also muck around with
    a .fixtures.yaml and the added rake spec_prep step from
    module_spec_helper and company, which maybe I have to.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,18 @@ fixture_path = File.expand_path(File.join(__dir__, 'fixtures'))
 
 RSpec.configure do |c|
   # Include the Bolt .modules directory as part of the modulepath for dependencies.
-  c.module_path     = [File.join(fixture_path, 'modules'), File.join(__dir__, '..', '.modules')].join(':')
+#  c.module_path     = [File.join(fixture_path, 'modules'), File.join(__dir__, '..', '.modules')].join(':')
+  c.module_path     = File.join(fixture_path, 'modules')
   c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
   c.environmentpath = File.join(Dir.pwd, 'spec')
+end
+
+require 'bolt_spec/plans'
+
+RSpec.shared_context 'plan_init' do
+  include BoltSpec::Plans
+
+  before(:all) do
+    BoltSpec::Plans.init
+  end
 end
